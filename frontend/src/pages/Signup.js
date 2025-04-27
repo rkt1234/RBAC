41e133d498a5686444488a04fc215f4b52b06d86
Ravikant Tiwari <ravikanttiwari@aesl.in>
2025-04-28 00:53:10 +0530

    removed the duplicate password field

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -36,7 +36,6 @@ const handleSubmit = async (e) => {
         <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
         <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
         <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-        <input type="" name="password" placeholder="Password" onChange={handleChange} required />
         <button type="submit">Signup</button>
       </form>
     </div>
